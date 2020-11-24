e7869695bb00bebe3d02ba71c5ce4f78b7a87c4e
John Brooks <johnbrooks@oakland.edu>
2020-11-24 00:55:18 -0500

    Finish up PI contol

--- Sources/main.c
+++ Sources/main.c
@@ -2,15 +2,77 @@
 #include "derivative.h"      /* derivative-specific definitions */
 #include "pi_control.h"
 
+#define UPDATE_RATE_S 0.5
+#define CLOCK_FREQ 24000000
+#define PRESCALER 128
+
+//returns the temperature in celcius of the thermocouple
+float GetTemperature()
+{
+
+}
+
+//sets the fan speed PWM (0.0 - 1.0)
+void SetFanSpeed(float pwm)
+{
+
+}
+
+void InitTimer()
+{
+	TSCR1 = 0x80; //Enable Timer
+	TSCR2 = 0x07; //Set prescaler to 128
+	TIOS = 0x40; //0100 0000 (channel 6 is outputs)
+}
+void PLLInit()
+{
+   SYNR = 2;
+   REFDV = 0;
+   PLLCTL = 0x60;  //PLLON = 1, High Bandwidth is selected in PLLCTL regsiter
+   while(!(CRGFLG & 0x08)); // wait until the PLL locks into the target frequency  
+   CLKSEL = 0x80; //PLLSEL = 1 FOR THE CLKSEL Regsiter
+}
+long ConvertTimeToCycles(float time, int prescaler)
+{
+	float tick_per_second = (float)CLOCK_FREQ / (float)prescaler;
+	return (long)(time * tick_per_second);
+}
+void DelayTicks(long ticks)
+{
+	unsigned int full_cycles = ticks / 65536;
+	unsigned int partial_cycle = ticks % 65536;
+	unsigned int current_cycle = 0;
+	TC6 = TCNT;
+	while (current_cycle++ < full_cycles)
+	{
+		TFLG1 = 0x40; //reset counter 6 flag
+		//wait out a full cycle of the 16-bit timer
+		while((TFLG1 & 0x40) == 0)
+			continue;
+	}
+
+	//wait out the partial cycle
+	TC6 += partial_cycle;
+	TFLG1 = 0x40; //reset counter 6 flag
+	while((TFLG1 & 0x40) == 0)
+		continue;
+}
+
+
 void main(void) {
 	Controller_t controller;
 	InitController(&controller);
-	
-
+	InitTimer();
+	PLLInit();
 
 	EnableInterrupts;
-
-
+	for(;;)
+	{
+		RunController(&controller, GetTemperature(), UPDATE_RATE_S);
+		SetFanSpeed(controller.output);
+		DelayTicks(UPDATE_RATE_S, PRESCALER);
+	}
+	
 	for(;;) {
 		_FEED_COP(); /* feeds the dog */
 	} /* loop forever */
--- Sources/pi_control.c
+++ Sources/pi_control.c
@@ -2,21 +2,36 @@
 
 void InitController(Controller_t* controller)
 {
-	controller->integral_term = 0.0f;
-	controller->proportional_term = 0.0f;
-	controller->set_point = 0.0f;
+	//If the temperature has been 10 degrees hotter than desired 
+	//for 10 seconds the accumulated error will be 100 "degree seconds"
+	//in this situation I would expect maybe an additional 5% fan duty cycle?
+	//Decreasing this coeff will make the controller less prone to overshoot.
+	controller->integral_coeff = 0.05 / 100; //5% per 100 degree seconds
+
+	//For every degree C scale fan speed by X percent.
+	controller->proportional_term = 5.0;
+
+	//maintain temperature of 40C
+	controller->set_point = 40;
+
 	controller->output = 0.0f;
 }
 
 void RunController(Controller_t* controller, float current_value, float delta_t)
 {
-	float error = controller->set_point - current_value;
-	controller->proportional_term = error * controller->proportional_coeff;
+	float error = current_value - controller->set_point;
 
+	controller->proportional_term = error * controller->proportional_coeff;
+	
 	//note that we assume the error does not change significantly between calls, if 
 	//we find that this function is called infrequently, or that the temperature 
 	//changes faster, then this line should actually average the error since the last call.
 	controller->integral_term += error * delta_t * controller->integral_coeff;
 
 	controller->output = controller->proportional_term + controller->integral_term;
+
+	if (controller->output < 0)
+		controller->output = 0;
+	else if (controller->output > 1)
+		controller->output = 1;
 }
